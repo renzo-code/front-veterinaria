83b33bdfee73c87a9b97588103817dde5c448592
RenzoNeira <renzoneira@hotmail.com>
2020-07-29 18:07:06 -0500

    creando componentes

---
+++ src/components/ComboBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const ComboBox = () => {
+  return (
+    <div>
+        <select name="" id="">
+          <option value="">Hola</option>
+          <option value="">Hola2</option>
+        </select>
+    </div>
+  )
+}
+
+export default ComboBox
--- src/pages/Web/Configuracion/Configuracion.jsx
+++ src/pages/Web/Configuracion/Configuracion.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ComboBox from './../../../components/ComboBox'
 
 const Configuracion = () => {
   return (
     <div>
       Configuracion
+      <div>
+        <ComboBox></ComboBox>
+      </div>
     </div>
   )
 }
